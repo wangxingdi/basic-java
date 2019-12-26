b4f0806e5ec933e3e6e5227b3cd2cd996b1e5fdb
王兴迪 <wangxingdi@boco.com.cn>
2019-12-26 18:23:50 +0800

    20191226

--- src/main/java/com/youyanpai/jdk/lang/math/MathTest.java
+++ src/main/java/com/youyanpai/jdk/lang/math/MathTest.java
@@ -3,10 +3,22 @@ package com.youyanpai.jdk.lang.math;
 public class MathTest {
 
 	public static void main(String[] args) {
-		//自然对数底数
+		//自然对数底数,2.718281828459045
 		System.out.println(Math.E);
-		//圆周率
+		//圆周率,3.141592653589793
 		System.out.println(Math.PI);
+		//二进制,15
+		System.out.println(0b1111);
+		//八进制,73
+		System.out.println(0111);
+		//十进制,111
+		System.out.println(111);
+		//十六进制,65535
+		System.out.println(0xffff);
+		//10的次幂(很少用)
+		System.out.println(2e1);
+		//
+		System.out.println(Math.abs(Float.NEGATIVE_INFINITY));
 	}
 
 }
