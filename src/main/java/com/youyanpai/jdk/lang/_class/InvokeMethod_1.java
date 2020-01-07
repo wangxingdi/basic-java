6364636a2807daf6bfedba447b82a5e5ab29ca45
王兴迪 <wangxingdi@boco.com.cn>
2020-01-07 09:46:00 +0800

    20200107

--- src/main/java/com/youyanpai/jdk/lang/_class/InvokeMethod_1.java
+++ src/main/java/com/youyanpai/jdk/lang/_class/InvokeMethod_1.java
@@ -3,14 +3,22 @@ package com.youyanpai.jdk.lang._class;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class InvokeMethod {
+/**
+ * 测试invoke();
+ * 低等级
+ * @author wangxd
+ */
+public class InvokeMethod_1 {
 
 	public static void main(String[] args)
-			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException, NoSuchMethodException, SecurityException {
+		SampleClass c1 = new SampleClass();
+		Method method = SampleClass.class.getMethod("setSampleField", String.class);
+		method.invoke(c1, "gg");
+		
+		SampleClass c2 = new SampleClass();
 		Method[] methods = SampleClass.class.getMethods();
-		SampleClass sampleObject = new SampleClass();
-		methods[1].invoke(sampleObject, "data");
-		System.out.println(methods[0].invoke(sampleObject));
+		methods[0].invoke(c2, "data");
 	}
 }
 
@@ -23,6 +31,7 @@ class SampleClass {
 
 	public void setSampleField(String sampleField) {
 		System.out.println("main方法中的invoke()将会唤醒对应的方法,也就是此方法");
+		System.out.println("入参是："+sampleField);
 		this.sampleField = sampleField;
 	}
 }
