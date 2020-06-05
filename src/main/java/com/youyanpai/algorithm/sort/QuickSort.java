6e5ec61a4ea3b5938cd450fb58aaaeb7920ce74f
王兴迪 <wangxingdi@boco.com.cn>
2020-06-05 18:08:34 +0800

    20200605

--- src/main/java/com/youyanpai/algorithm/sort/QuickSort.java
+++ src/main/java/com/youyanpai/algorithm/sort/QuickSort.java
@@ -14,8 +14,45 @@ public class QuickSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] arr = {7, 2, 1, 9, 6, 29, 74, 31, 124, 25, 35, 54, 85, 2, 24, 35, 68, 84, 51, 24, 67, 98};
-		System.out.println(arr);
+		int[] arr = {7, 2, 1, 9, 6, 29, 74, 31};
+		quickSort(arr, 0, arr.length-1);
+		for(int a : arr){
+			System.out.println(a);
+		}
+	}
+	
+	/**
+	 * 快速排序
+	 * @param arr 待排序数组
+	 * @param start 起始下标
+	 * @param stop 终止下标
+	 * @return
+	 */
+	public static void quickSort(int[] arr, int start, int stop){
+		if(start < stop){
+			int left = start;
+			int right = stop;
+			int tmp = arr[left];
+			while(left<right){
+				//从右向左,调走小于哨兵的元素
+				while(arr[right]>=tmp && left<right){
+					right--;
+				}
+				if(left<right){
+					arr[left++] = arr[right];
+				}
+				//从左向右,调走大于哨兵的元素
+				while(arr[left]<=tmp && left<right){
+					left++;
+				}
+				if(left<right){
+				    arr[right--] = arr[left];
+				}
+			}
+			arr[left] = tmp;
+			quickSort(arr, start, left - 1);
+			quickSort(arr, left + 1, stop);
+		}
 	}
 
 }
