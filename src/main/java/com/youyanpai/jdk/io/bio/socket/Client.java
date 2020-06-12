7c89f473d471abf82aa9b0c09c6a0b501d4787bc
wangxingdi <forwangxd@gmail.com>
2020-06-13 00:00:12 +0800

    20200613

--- src/main/java/com/youyanpai/jdk/io/bio/socket/Client.java
+++ src/main/java/com/youyanpai/jdk/io/bio/socket/Client.java
@@ -2,22 +2,39 @@ package com.youyanpai.jdk.io.bio.socket;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 
 	public static void main(String[] args) {
-		try(Socket client = new Socket("127.0.0.1", Server.PORT);
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-				BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()))){
-			writer.write("我是一条客户端发出的消息");
-			writer.flush();
-			System.out.println("收到服务器返回的消息: "+reader.readLine());
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		try {
+			for(int i=0;i<100;i++){
+		         Socket s = new Socket("127.0.0.1",8888);
+		         //构建IO
+		         InputStream is = s.getInputStream();
+		         OutputStream os = s.getOutputStream();
+		         
+		         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+		         //向服务器端发送消息
+	        	 bw.write("服务端你好"+ i +"\n");
+		         bw.flush();
+		         //读取服务器返回的消息
+		         BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		         String mess = br.readLine();
+		         System.out.println("服务器返回的消息："+mess);
+		         s.close();
+	         }
+	      } catch (UnknownHostException e) {
+	         e.printStackTrace();
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      }
 	}
 
 }
--- src/main/java/com/youyanpai/jdk/io/bio/socket/Server.java
+++ src/main/java/com/youyanpai/jdk/io/bio/socket/Server.java
@@ -1,6 +1,5 @@
 package com.youyanpai.jdk.io.bio.socket;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -13,51 +12,79 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * ServerSocket
+ * ServerSocket服务端
  * @author wangxd
  */
 public class Server {
-	
+
 	public static final int PORT = 8888;
 
 	public static void main(String[] args) throws IOException {
-		startServer();
+		startThreadPoolServer();
 	}
-	
-	public static void startServer() throws IOException{
-		//创建线程池
+
+	/**
+	 * 开启服务端
+	 * 没有使用线程池的方式
+	 */
+	public static void startServer() {
+		try {
+			ServerSocket server = new ServerSocket(8888);
+			System.out.println("启动服务器....");
+			Socket client = null;
+			while ((client = server.accept()) != null) {
+				System.out.println("客户端:" + client.getInetAddress().getLocalHost() + "已连接到服务器");
+				BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				// 读取客户端发送来的消息
+				String mess = br.readLine();
+				System.out.println("客户端发来的消息：" + mess);
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+				bw.write("客户端你好\n");
+				bw.flush();
+			}
+			client.close();
+			server.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 开启服务端
+	 * 使用线程池的方式
+	 * @throws IOException
+	 */
+	public static void startThreadPoolServer() throws IOException {
+		// 创建线程池
 		ThreadPoolExecutor pool = new ThreadPoolExecutor(10, 10, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(10));
-		//创建服务端
+		// 创建服务端
 		ServerSocket server = new ServerSocket(PORT);
 		server.setReceiveBufferSize(1024);
-		System.out.println("启动了一个ServerSocket: "+server.getInetAddress().getLocalHost());
-		Socket client = server.accept();
-		System.out.println("客户端: "+client.getInetAddress().getLocalHost()+" 已经连接到服务器");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		System.out.println("收到客户端信息: "+reader.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-        bw.write("收到客户端消息，同时返回一条消息");
-        bw.flush();
-//		pool.execute(new ReaderHandle(client));
-
+		Socket client = null;
+		System.out.println("启动了一个ServerSocket");
+		while ((client = server.accept())!=null) {
+			System.out.println("与客户端连接成功！");
+			pool.execute(new ReaderHandle(client));
+		}
+		server.close();
 	}
-	
+
 	static class ReaderHandle implements Runnable {
-		public Socket socket;
-		public ReaderHandle(Socket socket){
+		private Socket socket = null;
+		public ReaderHandle(Socket socket) {
 			this.socket = socket;
 		}
 
 		@Override
 		public void run() {
-			System.out.println("等待客户端接入…………");
-			byte[] bytes = new byte[1024];
-			int length = 0;
-			try(BufferedInputStream buff = new BufferedInputStream(socket.getInputStream());){
-				while((length=buff.read(bytes, 0, length))!=-1){
-					System.out.println(bytes);
-				}
-			}catch(Exception e){
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+				String s = reader.readLine();
+				System.out.println("当前线程ID: "+ Thread.currentThread().getId());
+				System.out.println("收到客户端信息: " + s);
+				writer.write("返回消息: "+ s + "\n");
+				writer.flush();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			try {
@@ -65,9 +92,7 @@ public class Server {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}	
+		}
 	}
-	
-	
 
 }
