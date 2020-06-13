cc71692b8bd5f7b7a1805b8a9db0bdba7a79b283
wangxingdi <forwangxd@gmail.com>
2020-06-13 17:02:30 +0800

    20200613

--- src/main/java/com/youyanpai/jdk/io/bio/stream/BufferedStreamIO.java
+++ src/main/java/com/youyanpai/jdk/io/bio/stream/BufferedStreamIO.java
@@ -15,24 +15,23 @@ import java.util.Date;
  */
 public class BufferedStreamIO {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		String inFile = "src/main/java/com/youyanpai/jdk/io/bio/stream/FileStreamBufferedIO.java";
 		String outFile = "src/main/java/com/youyanpai/jdk/io/bio/stream/FileStreamBufferedIOCopy.txt";
-		InputStream is = new FileInputStream(inFile);
-		OutputStream os = new FileOutputStream(outFile);
-		BufferedInputStream bis = new BufferedInputStream(is);
-		BufferedOutputStream bos = new BufferedOutputStream(os);
-		byte[] bytes = new byte[16];
-		int i = -1;
-		while((i=bis.read(bytes, 0, bytes.length)) != -1){
-			System.out.println("本次buffer读取的数据长度:"+i);
-			bos.write(bytes, 0, i);
+		try(InputStream is = new FileInputStream(inFile);
+				OutputStream os = new FileOutputStream(outFile);
+				BufferedInputStream bis = new BufferedInputStream(is);
+				BufferedOutputStream bos = new BufferedOutputStream(os)){
+			byte[] bytes = new byte[16];
+			int i = -1;
+			while((i=bis.read(bytes, 0, bytes.length)) != -1){
+				System.out.println("本次buffer读取的数据长度:"+i);
+				bos.write(bytes, 0, i);
+			}
+			bos.write(("当前系统时间:"+new Date().toString()).getBytes());
+		}catch(Exception e){
+			e.printStackTrace();
 		}
-		bos.write(("当前系统时间:"+new Date().toString()).getBytes());
-		bis.close();
-		bos.close();
-		is.close();
-		os.close();
 		System.out.println("===读写结束===");
 	}
 
