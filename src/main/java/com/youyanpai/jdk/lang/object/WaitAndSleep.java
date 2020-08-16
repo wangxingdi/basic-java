9e01dec671221ce9777bc6469e72cb62ec247ff0
wangxingdi <forwangxd@gmail.com>
2020-08-16 22:58:03 +0800

    20200816

--- src/main/java/com/youyanpai/jdk/lang/object/WaitAndSleep.java
+++ src/main/java/com/youyanpai/jdk/lang/object/WaitAndSleep.java
@@ -19,8 +19,11 @@ public class WaitAndSleep {
             new Thread(n) {
                 public void run(){
                     try {
+                        //下面的方法建议一个一个的执行
                         waitWhetherReleaseLock();
                         sleepWhetherReleaseLock();
+                        waitWithoutSync();
+                        sleetWithoutSync();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -29,6 +32,10 @@ public class WaitAndSleep {
         });
     }
 
+    /**
+     * 线程1休眠后,线程2无法进入同步方法,直到线程1执行完毕;
+     * @throws InterruptedException
+     */
     private static void sleepWhetherReleaseLock() throws InterruptedException {
         synchronized(lock){
             System.out.println(Thread.currentThread().getName()+"正在执行");
@@ -37,12 +44,37 @@ public class WaitAndSleep {
         }
     }
 
+    /**
+     * 线程1休眠后,线程2可以进入同步方法;
+     * @throws InterruptedException
+     */
     private static void waitWhetherReleaseLock() throws InterruptedException {
         synchronized(lock){
             System.out.println(Thread.currentThread().getName()+"正在执行");
             lock.wait(3000);
             System.out.println(Thread.currentThread().getName()+"执行结束");
         }
+    }
+
+    /**
+     * 如果不使用synchronized,wait方法会出现IllegalMonitorStateException
+     * @throws InterruptedException
+     */
+    private static void waitWithoutSync() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName()+"正在执行");
+        lock.wait(3000);
+        System.out.println(Thread.currentThread().getName()+"执行结束");
+    }
+
+    /**
+     * 如果不使用synchronzied,sleep方法仍旧可以执行,
+     * 只不过在线程1和线程2不会再同步执行
+     * @throws InterruptedException
+     */
+    private static void sleetWithoutSync() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName()+"正在执行");
+        Thread.sleep(3000);
+        System.out.println(Thread.currentThread().getName()+"执行结束");
     }
 
 
