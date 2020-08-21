70aba7042fc32f31dd02b852b46e29dde403f3b4
ÍõÐ˵Ï <wangxingdi@boco.com.cn>
2020-08-21 17:02:06 +0800

    20200821

---
+++ src/main/java/com/youyanpai/tmp/day_20200819/_String.java
@@ -0,0 +1,12 @@
+package com.youyanpai.tmp.day_20200819;
+
+public class _String {
+
+    public static void main(String[] args) {
+        //形式1
+        String s1 = "abc";
+        //形式2
+        String s2 = "a" + "b" + "c";
+        System.out.println(s1 == s2);
+    }
+}
