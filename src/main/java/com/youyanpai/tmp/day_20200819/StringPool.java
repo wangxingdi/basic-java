79f2254e4de4f02732bd695077c0f2cdb5f38013
ÍõÐ˵Ï <wangxingdi@boco.com.cn>
2020-09-05 18:21:28 +0800

    20200905

---
+++ src/main/java/com/youyanpai/tmp/day_20200819/StringPool.java
@@ -0,0 +1,8 @@
+package com.youyanpai.tmp.day_20200819;
+
+public class StringPool {
+    public static String ss = "gg";
+    public static void main(String[] args) {
+//        String s = "hello, world";
+    }
+}
