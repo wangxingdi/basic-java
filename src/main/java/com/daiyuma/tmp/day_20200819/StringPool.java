098cf6409cb1e50b96aa5247a8bbb9ded9ec07b8
ÍõÐ˵Ï <wangxingdi@boco.com.cn>
2020-09-07 19:20:43 +0800

    2020090701

--- src/main/java/com/daiyuma/tmp/day_20200819/StringPool.java
+++ src/main/java/com/daiyuma/tmp/day_20200819/StringPool.java
@@ -1,11 +1,16 @@
 package com.daiyuma.tmp.day_20200819;
 
+/**
+ * -XX:+PrintStringTableStatistics
+ */
 public class StringPool {
-    public static String ss = "gg";
+//    public static String ss = "gg";
     public static void main(String[] args) {
-        String str1 = "hello";
-        String s = ss + str1;
+        final String str1 = "hello";
+        String s = "aaa" + str1;
 //        String s1 = new String("sss");
 //        String s2 = new String("s") + new String("ss");
+//        System.out.println(s1);
+        System.out.println(s);
     }
 }
